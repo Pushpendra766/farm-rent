2ee65fc0470f4e9b9da7d455043e5272372abb5d
Pushpendra Chandravanshi <pushpendrachandravanshi4@gmail.com>
2023-02-13 19:01:36 +0530

    integrated tailwind css

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      primary: "#379237",
+      secondary: "#54B435",
+      mid: "#82CD47",
+      light: "#F0FF42",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
