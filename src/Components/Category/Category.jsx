b4e4efbeff13616f660046b960925fff79110e01
Anekant <Anekantjainsagar@gmail.com>
2023-02-14 22:22:44 +0530

    Added category page

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useLocation } from "react-router";
 
 const Category = () => {
+  const location = useLocation();
+  const ref = useRef();
+
+  useEffect(() => {
+    if (location.pathname.includes("Category")) {
+      ref.current.classList.add("w-10/12");
+      ref.current.classList.add("mx-auto");
+      ref.current.classList.add("my-5");
+      ref.current.classList.remove("w-2/12");
+    }
+  }, [location]);
+
   return (
     <div
-      style={{
-        width: "14.25vw",
-        height: "90%",
-      }}
-      className="rounded-md border-1 border-lightest-grey mx-2.5 cursor-pointer transition-all hover:scale-105 shrink-0"
+      ref={ref}
+      className=" self-center w-2/12 my-3 rounded-md border-1 py-2 border-lightest-grey mx-2.5 cursor-pointer transition-all hover:scale-105 shrink-0"
     >
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_8xB9u3Fg0HvPxY23zCzu0P5dL_g-DUtxeA&usqp=CAU"
@@ -18,7 +28,7 @@ const Category = () => {
         <p className="text-sm text-dark-green">
           Rs. 499 <span className="line-through text-orange">899</span>
         </p>
-        <p className="text-xs text-center px-2">
+        <p className="text-sm text-center px-2">
           {"A tractor is a vehicle that's used on a farm or work site, often to pull a trailer or other equipment.".slice(
             0,
             50
--- src/Screens/Category/CategoryScreen.jsx
+++ src/Screens/Category/CategoryScreen.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useParams } from "react-router";
+
+import Category from "../../Components/Category/Category";
 
 const CategoryScreen = () => {
-  return <div>CategoryScreen</div>;
+  const params = useParams();
+  return (
+    <div className="w-full my-5">
+      <h1 className="text-center text-3xl font-semibold text-dark-green mt-1">
+        Best of {params.category}
+      </h1>
+      <p className="text-center text-base mb-1 text-orange">103 Items</p>
+      <div className="grid grid-cols-4 w-10/12 mx-auto">
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+      </div>
+    </div>
+  );
 };
 
 export default CategoryScreen;
