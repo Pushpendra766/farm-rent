2bb59d9900bf064c6357fa1038ef3d4d2ace40f8
Pushpendra Chandravanshi <pushpendrachandravanshi4@gmail.com>
2023-02-16 13:22:14 +0530

    product

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
 import "./styles/styles.css";
 import { Route, Routes } from "react-router";
-
 import CategoryScreen from "./Screens/Category/CategoryScreen";
-
 import Home from "./Screens/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
+import Product from "./Screens/Product/Product";
 
 function App() {
   return (
@@ -15,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/category/:category" element={<CategoryScreen />} />
+        <Route path="/product/:id" element={<Product />} />
       </Routes>
       <Footer />
     </div>
