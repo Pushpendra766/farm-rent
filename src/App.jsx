2bdae738186085f8dfc60f68fbd5a7ed8e800301
Shivam <shivam.yadav98939294@gmail.com>
2023-02-17 15:34:40 +0530

    commiting work done

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,6 @@ function App() {
     <div className="App">
       <Navbar/>
       <Itemsbar/>
-      <img src="https://o.remove.bg/downloads/0ac4a62a-bccd-4f5d-946d-9e0a0fef7a24/pngtree-farm-house-png-image_5412129-removebg-preview.png"/>
-     
       <CategorySection />
       <CategorySection />
       <CategorySection />
