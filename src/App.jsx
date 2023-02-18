083cd5a16af1f69dd8238270f64772b7bb56fe85
Shivam <shivam.yadav98939294@gmail.com>
2023-02-18 23:54:50 +0530

    commiting

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,29 @@ import { Route, Routes } from "react-router";
 import CategoryScreen from "./Screens/Category/CategoryScreen";
 import Home from "./Screens/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
-import CategorySection from "./Components/CategorySection/CategorySection";
+import Itemsbar from  "./Components/Itemsbar/Itemsbar"
 import Footer from "./Components/Footer/Footer"
+import Intro from "./Components/Intro/Intro";
+import HowItWorks from "./Components/HowItWorks/HowItWorks";
+import Testimonials from "./Sections/Testimonials/Testimonials";
+import Faq from "./Sections/Faq/Faq";
+import Trending from "./Sections/Trending/Trending";
+
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <CategorySection />
-      <CategorySection />
-      <CategorySection />
-      <Footer/>
+      <Navbar />
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:category" element={<CategoryScreen />} />
+        <Route path="/product/:id" element={<Product />} />
+      </Routes> */}
+       <Itemsbar/>
+      <Intro />
+      <Trending/>
+      <Testimonials />
+      <Faq />
+      <Footer />
     </div>
   );
 }
